7a082d75c0bcf61d7ee744a7041393c31011e4ab
VPyatakov <pyatakov@skbkontur.ru>
2023-09-15 20:33:14 +0300

    add three endpoints for upload, download files and method to get list of files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9
+FROM python:3.10
 
 WORKDIR /code
 
@@ -6,8 +6,6 @@ COPY ./requirements.txt /code/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-# 
 COPY ./app /code/app
 
-# 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- app/main.py
+++ app/main.py
@@ -1,16 +1,52 @@
-from typing import Union
+import logging
+import os
+import shutil
+from uuid import UUID
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.responses import FileResponse
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+log = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/api/files")
+async def get_files():
+    response = []
+    for file in os.listdir(UPLOAD_DIR):
+        guid, file_name = file.split("--")
+        response.append({
+            "guid": guid,
+            "file_name": file_name
+        })
+    return response
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    a = 10
-    return {"item_id": item_id, "q": q}
+@app.post("/api/file")
+async def upload(guid: UUID = Form(...), file: UploadFile = File(...)):
+    with open(f"{UPLOAD_DIR}/{guid}--{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"username": "name"}
+
+
+@app.get("/api/file")
+async def download(guid: UUID):
+    log.info(guid)
+
+    file_name = None
+    for file in os.listdir(UPLOAD_DIR):
+        if str(guid) in file:
+            file_name = file
+
+    if file_name:
+        return FileResponse(f"{UPLOAD_DIR}/{file_name}",
+                            media_type="application/octet-stream",
+                            filename=file_name.split(str(guid))[1],
+                            )
+    else:
+        return {"error": "No such file in given guid"}
